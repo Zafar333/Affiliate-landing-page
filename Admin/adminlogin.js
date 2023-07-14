6c38cd12bf767cb0a17b99ac7381010b4be1e305
Arslan Zafar <arslanz499@gmail.com>
2023-07-14 23:27:20 +0500

    done by arslan

--- Admin/adminlogin.js
+++ Admin/adminlogin.js
@@ -1,28 +1,27 @@
-// import axios from "https://unpkg.com/axios/dist/axios.min.js";
-// var AdminFormData;
+var AdminFormData;
+function readdInput(event) {
+  const { name, value } = event.target;
+  AdminFormData = { ...AdminFormData, [name]: value };
+  console.log(AdminFormData);
+}
+const AdminPostReq = async () => {
+  let usernameRef = document.querySelector(".username");
+  let passwordRef = document.querySelector(".password");
 
-// const AdminPostReq = async () => {
-//   let usernameRef = document.querySelector(".username");
-//   let passwordRef = document.querySelector(".password");
-
-//   try {
-//     const response = await axios.post(
-//       "http://localhost:5000/auth/adminLogin",
-//       AdminFormData
-//     );
-//     if (response.data.status == 200) {
-//       localStorage.setItem("token", response.data.token);
-//       window.location.href = "../AdminDashboard/airlinePage.html";
-//     } else {
-//       alert("Plese Enter Correct username and password");
-//     }
-//   } catch (error) {
-//     alert(error);
-//   }
-//   usernameRef.value = "";
-//   passwordRef.value = "";
-// };
-// function readAdminInput(event) {
-//   const { name, value } = event.target;
-//   AdminFormData = { ...AdminFormData, [name]: value };
-// }
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/auth/adminLogin",
+      AdminFormData
+    );
+    if (response.data.status == 200) {
+      localStorage.setItem("token", response.data.token);
+      window.location.href = "../AdminDashboard/airlinePage.html";
+    } else {
+      alert("Plese Enter Correct username and password");
+    }
+  } catch (error) {
+    alert(error);
+  }
+  usernameRef.value = "";
+  passwordRef.value = "";
+};
